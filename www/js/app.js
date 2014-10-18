f5a2ff121a79068398afe918443fb348f19d2a56
Sheehan Toufiq <sheehantoufiq@gmail.com>
2014-10-18 19:05:53 -0400

    Buggy stuff

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,17 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'CrowdJockey' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'CrowdJockey.services' is found in services.js
+// 'CrowdJockey.controllers' is found in controllers.js
+var app = angular.module('CrowdJockey', [
+  'ionic', 
+  'CrowdJockey.controllers', 
+  'CrowdJockey.services'
+]);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -19,66 +23,24 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       StatusBar.styleDefault();
     }
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
-        }
-      }
-    })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
-    });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
 });
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/', templateUrl: 'templates/home.html', controller: 'HomeController'
+  });
+  $stateProvider.state('create', {
+    url: '/create', templateUrl: 'templates/create.html', controller: 'CreateController'
+  });
+  $stateProvider.state('trending', {
+    url: '/trending', templateUrl: 'templates/trending.html', controller: 'TrendingController'
+  });
+  $stateProvider.state('chat', {
+    url: '/:name', templateUrl: 'templates/crowdchat.html', controller: 'ChatroomController'
+  });
+  $stateProvider.state('test', {
+    url: '/test', templateUrl: 'templates/test.html', controller: 'TestController'
+  });
+
+  $urlRouterProvider.otherwise("/");
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,16 @@
-angular.module('starter.controllers', [])
+var app = angular.module('CrowdJockey.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-})
+app.controller('HomeController', function($scope) {
+});
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
+app.controller('CreateController', function($scope) {
+});
 
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
+app.controller('ChatroomController', function($scope) {
+});
 
-.controller('AccountCtrl', function($scope) {
+app.controller('TrendingController', function($scope) {
+});
+
+app.controller('TestController', function($scope) {
 });
